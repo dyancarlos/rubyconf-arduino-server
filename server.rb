7d0174cbd0a2e6ce06a7e009c8b6b34341a6a78a
Dyan Carra <dyancarra@gmail.com>
2017-11-17 22:25:59 -0200

    minors

--- server.rb
+++ server.rb
@@ -1,13 +1,13 @@
-require "artoo"
+require 'artoo'
 require 'pusher-client'
 
 def serial_port
   ports = %x(ls /dev/cu.*).split
-  ports.delete_if { |p| p.include?("Bluetooth") }
+  ports.delete_if { |p| p.include?('Bluetooth') }
   ports.first
 end
 
-socket = PusherClient::Socket.new("66c13dbdccd127e0e125", { encrypted: true })
+socket = PusherClient::Socket.new('66c13dbdccd127e0e125', { encrypted: true })
 socket.subscribe('light')
 
 connection :arduino, adaptor: :firmata, port: serial_port
@@ -15,7 +15,7 @@ device     :light,   driver:  :led,     pin: 8
 
 work do
   socket.bind('change-status') do |data|
-    if data == "on"
+    if data == 'on'
       light.on
     else
       light.off
@@ -24,5 +24,3 @@ work do
 
   socket.connect
 end
-
-
